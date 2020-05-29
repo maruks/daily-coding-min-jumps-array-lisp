4896546b6e3dbb37213a2eff58b6d71cd378d3c5
Maris Orbidans <maris.orbidans@onthemarket.com>
2020-05-29 01:01:24 +0100

    refactor

--- src/min-jumps-in-array.lisp
+++ src/min-jumps-in-array.lisp
@@ -25,31 +25,33 @@
 ;; --------------------------------- O(n) ---------------------------------
 
 (defun find-min-jumps (vec index steps max-reach result)
-  (if (>= index (1- (length vec)))
-       result
-      (let ((new-max-reach (max max-reach (+ index (svref vec index)))))
-	(if (zerop steps)
-	    (let ((next-steps (- new-max-reach index)))
-	      (when (plusp next-steps)
-		(find-min-jumps vec (1+ index) (1- next-steps) new-max-reach (1+ result))))
-	    (find-min-jumps vec (1+ index) (1- steps) new-max-reach result)))))
+  (if (> index (1- (length vec)))
+      result
+      (when (<= index max-reach)
+	(let ((jump? (zerop steps)))
+	  (find-min-jumps
+	   vec
+	   (1+ index)
+	   (if jump? (- max-reach index) (1- steps))
+	   (max max-reach (+ index (svref vec index)))
+	   (if jump? (1+ result) result))))))
 
 (defun min-jumps-2 (vec)
   (when (and (plusp (length vec)) (plusp (svref vec 0)))
-    (find-min-jumps vec 1 (1- (svref vec 0)) (svref vec 0) 1)))
+    (find-min-jumps vec 1 (svref vec 0) (svref vec 0) 1)))
 
 (defun min-jumps-3 (vec)
   (when (and (plusp (length vec)) (plusp (svref vec 0)))
     (iter
-      (with steps = (1- (svref vec 0)))
+      (with steps = (svref vec 0))
       (with max-reach = (svref vec 0))
       (with result = 1)
-      (for index :from 1 to (- (length vec) 2))
+      (for index :from 1 :to (1- (length vec)))
+      (always (<= index max-reach))
+      (if (zerop steps)
+	  (progn
+	    (setf steps (- max-reach index))
+	    (incf result))
+	  (decf steps))
       (setf max-reach (max max-reach (+ index (svref vec index))))
-      (when (zerop steps)
-	  (let ((next-steps (- max-reach index)))
-	    (always (plusp next-steps))
-	    (setf steps next-steps)
-	    (incf result)))
-      (decf steps)
       (finally (return result)))))
